f1f603991995c676d010addd18b42f7000955a4b
Ian <iantevesacc@gmail.com>
2023-01-25 09:13:22 +1300

    Project Base
    Uploading Word doc and C++ Programme to contain understanding of OOP

---
+++ OOP Study/OOP Study.cpp
@@ -0,0 +1,22 @@
+// Test Run
+/*
+#include <iostream>
+#include <string>
+
+int main() {
+	//declare str var
+	std::string name;
+	char fullName[20];
+
+	//ask user their name
+	std::cout << "What is your name user? ";
+	getline(std::cin, name);
+	
+	//greet user
+	std::cout << "Hello " << name << std::endl;
+
+	return 0;
+}
+*/
+
+// 
